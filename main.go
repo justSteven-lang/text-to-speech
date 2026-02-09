920d7e557e917a4942fa2aa29053d0b14e28f28a
Steven Oktavian <stevenoktavian45@gmail.com>
2026-02-09 14:20:04 +0700

    feat: simple text to speech simulation

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func main() {
-	fmt.Println("Text to Speech - Step One")
+func textToSpeech(text string) error {
+	file, err := os.Create("output.txt")
+	if err != nil {
+		return  err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString("AUDIO_SIMULATION: " + text)
+	return err
+}
+
+func main()  {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go \"your text\"")
+		return
+	}
+
+	text := os.Args[1]
+
+	err := textToSpeech(text)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Text converted to audio (simulated). File: output.txt")
 }
